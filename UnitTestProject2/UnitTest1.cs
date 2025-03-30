e6a52d073daf502220ce47f45b53b9320b73b57a
migosanser <amigosanser@yandex.ru>
2025-03-30 23:09:01 +0300

    UpData2

--- UnitTestProject2/UnitTest1.cs
+++ UnitTestProject2/UnitTest1.cs
@@ -1,50 +1,87 @@
 ﻿using System;
-using System.Windows;
-using System.Windows.Controls;
-using Courswork.Pages;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Courswork;
+using System.Security.Cryptography;
+using System.Text;
+using System.Linq;
 
 namespace UnitTestProject2
 {
     [TestClass]
-    public class UnitTest1
+    public class LoginPageTests
     {
-        [TestMethod]
-        private void EntrButton_Click(object sender, RoutedEventArgs e)
-        {
-            Auth(TextBoxlogin.Text, PasswordBox.Password);
-        }
-        [TestMethod]
-        public bool Auth(string login, string password)
-        {
-            if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
-            {
-                MessageBox.Show("Введите логин и пароль!");
-                return false;
-            }
-            using (var db = new Entities())
-            {
-                var user = db.Users.AsNoTracking().FirstorDefault(u => u.Login == login && u.Password == password);
+        private const string TestUsername = "testuser";
+        private const string TestPassword = "testpass123";
+        private string _credentialsFilePath;
 
-                if (user == null)
-                {
-                    MessageBox.Show("Пользователь с такими данными не найден!");
-                    return false;
-                }
-                MessageBox.Show("Пользователь успешно найден!");
-                TextBoxLogin.Clear();
-                PasswordBox.Clear();
-                return true;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _credentialsFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "login_credentials_test.txt");
+
+            if (File.Exists(_credentialsFilePath))
+            {
+                File.Delete(_credentialsFilePath);
             }
         }
-        [TestMethod]
-        public void TestMethod1()
+
+        [TestCleanup]
+        public void TestCleanup()
         {
-            var page = new AuthPage();
-            Assert.IsTrue(page.Auth("test", "test"));
-            Assert.IsFalse(page.Auth("user1", "12345"));
-            Assert.IsFalse(page.Auth("", ""));
-            Assert.IsFalse(page.Auth(" ", " "));
+            if (File.Exists(_credentialsFilePath))
+            {
+                File.Delete(_credentialsFilePath);
+            }
+        }
+
+        [TestMethod]
+        public void GetHash_ReturnsCorrectSHA1Hash()
+        {
+            var input = "password123";
+            var expectedHash = ComputeExpectedSHA1Hash(input);
+
+            var actualHash = LoginPage.GetHash(input);
+
+            Assert.AreEqual(expectedHash, actualHash);
+        }
+
+        [TestMethod]
+        public void SaveCredentials_CreatesFileWithCorrectContent()
+        {
+            var loginPage = new LoginPage();
+            var privateObject = new PrivateObject(loginPage);
+            privateObject.SetField("_credentialsFilePath", _credentialsFilePath);
+
+            privateObject.Invoke("SaveCredentials", TestUsername, TestPassword);
+
+            Assert.IsTrue(File.Exists(_credentialsFilePath));
+            var lines = File.ReadAllLines(_credentialsFilePath);
+            Assert.AreEqual(2, lines.Length);
+            Assert.AreEqual(TestUsername, lines[0]);
+            Assert.AreEqual(TestPassword, lines[1]);
+        }
+
+        [TestMethod]
+        public void ClearCredentials_RemovesFile()
+        {
+            File.WriteAllLines(_credentialsFilePath, new[] { TestUsername, TestPassword });
+            var loginPage = new LoginPage();
+            var privateObject = new PrivateObject(loginPage);
+            privateObject.SetField("_credentialsFilePath", _credentialsFilePath);
+
+            privateObject.Invoke("ClearCredentials");
+
+            Assert.IsFalse(File.Exists(_credentialsFilePath));
+        }
+
+        private string ComputeExpectedSHA1Hash(string input)
+        {
+            using (var sha1 = SHA1.Create())
+            {
+                var hashBytes = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                return string.Concat(hashBytes.Select(b => b.ToString("X2")));
+            }
         }
     }
 }
